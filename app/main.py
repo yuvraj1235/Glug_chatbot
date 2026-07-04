842400c786de36d875f816090d6bbe652be7c1dd
nishgit17 <nishantgit17@gmail.com>
2026-07-05 03:55:20 +0530

    localhost allowed for dev

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from app.routes import chat, ingest, scrap
+from fastapi.middleware.cors import CORSMiddleware
 import os
 import logging
 from dotenv import load_dotenv
@@ -13,6 +14,21 @@ logging.basicConfig(
 
 # This is the "app" that Uvicorn is looking for!
 app = FastAPI(title="GLUG Chatbot API")
+
+origins = [
+    "http://localhost:3000",  
+    "http://localhost:5173",  
+    "http://127.0.0.1:5173",  
+    "http://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware, 
+    allow_origins=origins,   
+    allow_credentials=True,  
+    allow_methods=["*"],     
+    allow_headers=["*"],     
+)
 
 # Attach your routers to the main app
 app.include_router(chat.router)
